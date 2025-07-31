b49044a7c7fceabf2437e5903e01d7ac78c23977
Lngngyn289 <longnguyenduc98921@gmail.com>
2025-07-31 16:54:42 +0700

    be/ base structure & database

---
+++ backend/src/prisma/prisma.service.ts
@@ -0,0 +1,22 @@
+import { Injectable, OnModuleInit, OnModuleDestroy } from "@nestjs/common";
+import { PrismaClient } from "@prisma/client";
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+
+  // Optional: clean up DB for testing
+  async cleanDatabase() {
+    // await this.user.deleteMany();
+    // await this.booking.deleteMany();
+  }
+}
